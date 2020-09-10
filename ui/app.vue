c67b6a9a868f3add31705c8c83b93f4711d42cca
钟威 <zhongwei@dm-ai.cn>
2020-09-10 22:45:44 +0800

    refactor UI.

--- ui/app.vue
+++ ui/app.vue
@@ -11,7 +11,10 @@
 
     <v-spacer></v-spacer>
 
-    rightmost
+    <v-btn href="https://approach0.xyz" target="_blank" color="purple darken-1" dark>
+      <v-icon>storefront</v-icon> &nbsp; Main Site
+    </v-btn>
+
     </template>
   </v-app-bar>
 
@@ -21,13 +24,13 @@
         <v-text-field v-model="job_input" label="Run job" append-icon="sports" @click:append="clickRun" filled clearable>
         </v-text-field>
 
-        <v-card class="grey darken-4 console" v-bind:loading="console_loading">{{console_content}}</v-card>
-
         <v-row>
-          <v-chip class="ma-2" color="purple white--text">
-            <v-icon>star</v-icon> Hello
+          <v-chip class="ma-2" color="purple white--text" v-for="jobname in console_starjob" :key="jobname">
+            <v-icon>star</v-icon> &nbsp; {{jobname}}
           </v-chip>
         </v-row>
+
+        <v-card class="grey darken-4 console" v-bind:loading="console_loading">{{console_content}}</v-card>
 
     </v-container>
   </v-navigation-drawer>
@@ -40,7 +43,9 @@
   </v-main>
 
   <v-footer color="deep-purple" dark app>
-  footer
+    <v-row justify="center">
+      <strong>Approach0</strong>&nbsp; - &nbsp; {{ new Date().getFullYear() }}
+    </v-row>
   </v-footer>
 
 </v-app>
@@ -56,7 +61,7 @@ export default {
       drawer: false,
       job_input: '',
       console_loading: false,
-      console_jobname: 'master',
+      console_starjob: ['_master_'],
       console_content: '',
       debug: false,
     }
@@ -71,7 +76,8 @@ export default {
   mounted: function () {
     let vm = this
     setInterval(function () {
-      vm.fetch_log()
+      const recent_job = vm.console_starjob[0]
+      vm.fetch_log(recent_job)
     }, 1000)
   },
 
@@ -80,21 +86,27 @@ export default {
       return Math.floor(Math.random() * L)
     },
 
-    fetch_log() {
+    fetch_log(jobname) {
       let vm = this
       vm.console_loading = true
 
-      console.log(`http://0.0.0.0:${port}/get/log/_master_`)
-      axios.get(`http://0.0.0.0:${port}/get/log/_master_`)
+      axios.get(`http://0.0.0.0:${port}/get/log/${jobname}`)
       .then(function (res) {
         const data = res.data
         vm.console_content = data['logdata']
-        vm.console_jobname = data['jobname']
         vm.console_loading = false
+
+        var index = vm.console_starjob.indexOf(jobname)
+        if (index === -1) {
+          vm.console_starjob.push(jobname)
+        } else {
+          vm.console_starjob.splice(index, 1)
+          vm.console_starjob.unshift(jobname)
+        }
       })
       .catch(function (err) {
-        console.log(err);
-      });
+        console.log(err)
+      })
     },
 
     clickRun() {
@@ -108,6 +120,6 @@ export default {
   white-space: pre-wrap;
   overflow: auto;
   font-size: 14px;
-  max-height: 900px;
+  height: 85vh;
 }
 </style>
