a8dcddda03793754111573e499bb4bd18ed5a0bb
钟威 <zhongwei@dm-ai.cn>
2020-09-10 11:55:30 +0800

    implemented job runner.

--- jobd/job-runner.js
+++ jobd/job-runner.js
@@ -1,5 +1,9 @@
 const jobs_ldr = require('./jobs-ldr.js')
 const logger = require('./logger.js')
+const extend = require('util')._extend
+const process_ = require('process')
+const pty = require('node-pty-prebuilt-multiarch')
+const childProcess = require('child_process')
 
 function masterLog(data) {
   logger.write('_master_', data)
@@ -35,13 +39,142 @@ exports.syncLoop = function (arr, doing, done) {
   loop.again()
 }
 
-exports.getRunList = async function (jobs, target, user) {
+exports.getRunList = async function (jobs, target) {
   const deps = jobs.depGraph.dependenciesOf(target)
   deps.push(target)
   return deps
 }
 
-exports.runlist = function (jobs, runList, user, _dryrun, onComplete) {
+exports.spawn = function (cmd, opt, onLog, onSpawn, onExit)
+{
+  const pid = 0
+  const invoke_time = Date.now()
+
+  /* choose between pty.spawn and childProcess.spawn */
+  let spawnFun = pty.spawn
+  let stdout = function (o) {return o}
+  let stderr = function (o) {return {'on': function (d) {}}}
+  let stdin  = function (o) {return o}
+  let stdlog = function (o) {onLog(o)}
+
+  if (opt.spawn == 'direct') {
+    spawnFun = childProcess.spawn
+    stdout = function (o) {return o.stdout}
+    stderr = function (o) {return o.stderr}
+    stdin  = function (o) {return o.stdin}
+    stdlog = function (o) {onLog(o.toString())}
+  } else if (opt.spawn != 'pty') {
+    onLog('[ error ] spawn method unrecognized.')
+  }
+
+  /* get gid/uid numbers */
+  let uid = 0
+  let gid = 0
+  if (opt.user != 'root') {
+    uid = process_.getuid()
+    gid = process_.getgid()
+  }
+
+  return new Promise((resolve, reject) => {
+    /* spawn runner process */
+    const runner = spawnFun('/bin/sh', ['-c', cmd], {
+      uid, gid,
+      'cwd': opt.cwd,
+      'env': opt.env,
+      'cols': 80,
+      'rows': 30,
+      'name': 'xterm-color'
+    })
+
+    /* invoke onSpawn */
+    onSpawn(cmd, runner.pid)
+
+    /* pipe stdin into this process */
+    process.stdin.pipe(stdin(runner))
+
+    /* output std & stderr */
+    stdout(runner).on('data', stdlog)
+    stderr(runner).on('data', stdlog)
+
+    /* error handler for std & stderr */
+    stdout(runner).on('error', function () {})
+    stderr(runner).on('error', function () {})
+    stdin(runner).on('error', function () {})
+
+    /* on exit ... */
+    runner.on('exit', function (exitcode) {
+      process.stdin.unpipe(stdin(runner))
+      process.stdin.resume()
+      process.stdin.pause()
+
+      /* callback */
+      onExit(cmd, exitcode)
+    })
+  })
+}
+
+exports.runjob = async function (jobs, jobname, onSpawn, onExit, onAbort) {
+  const targetProps = jobs.depGraph.getNodeData(jobname)
+  const cmd = targetProps['exe'] || ''
+  const cwd = targetProps['cwd'] || '.'
+  const user = targetProps['user'] || 'unknown'
+  const spawn = targetProps['spawn'] || 'direct'
+
+  if (cmd === '') {
+    onExit(cmd, 0)
+    return
+  }
+
+  /* prepare spawn environment */
+  const onLog = function (lines) {
+    lines.split('\n').forEach(function (line) {
+      slaveLog(jobname, line)
+    })
+  }
+
+  const defaultEnv = {
+    'PATH': process.env['PATH'],
+    'USER': user,
+    'USERNAME': user,
+    'HOME': (user == 'root') ? '/root' : '/home/' + user,
+    'SHELL': '/bin/sh'
+  }
+  const jobEnv = jobs['envs'] || {}
+  const allEnv = extend(defaultEnv, jobEnv)
+
+  const opts = {
+    'env': allEnv,
+    'cwd': cwd,
+    'user': user,
+    'group': user,
+    'spawn': spawn
+  }
+
+  /* main command */
+  if (targetProps['if']) {
+    const ifcmd = targetProps['if']
+    await exports.spawn(ifcmd, opts, onLog, onSpawn, function (_, exitcode) {
+      if (exitcode != 0) {
+        onAbort()
+        return
+      }
+    })
+
+  } else if (targetProps['if_not']) {
+    const incmd = targetProps['if_not']
+    await exports.spawn(incmd, opts, onLog, onSpawn, function (_, exitcode) {
+      if (exitcode == 0) {
+        onAbort()
+        return
+      }
+    })
+  }
+
+  /* main command */
+  exports.spawn(cmd, opts, onLog, onSpawn, onExit)
+}
+
+exports.runlist = function (jobs, runList, _dryrun, onComplete) {
   const dryrun = _dryrun || false
 
   /* main loop */
@@ -56,7 +189,7 @@ exports.runlist = function (jobs, runList, user, _dryrun, onComplete) {
       let ref = props['ref']
       if (ref) {
         let subList = exports.getRunList(jobs, ref)
-        exports.runlist(jobs, subList, user, dryrun, (completed) => {
+        exports.runlist(jobs, subList, dryrun, (completed) => {
           if (completed)
             loop.next()
           else
@@ -66,13 +199,13 @@ exports.runlist = function (jobs, runList, user, _dryrun, onComplete) {
       }
 
       /* prepare process callbacks */
-      const onSpawn = function (jobname, pid) {
+      const onSpawn = function (cmd, pid) {
           slaveLog(jobname, `[ spawn ] ${jobname}, pid=${pid}.`)
       }
 
       let failcnt = 0
-      const onExit = function (jobname, exitcode) {
-        slaveLog(jobname, `[ exitcode ] ${exitcode}`)
+      const onExit = function (cmd, exitcode) {
+        slaveLog(jobname, `[ exitcode = ${exitcode} ] ${cmd}\n`)
         if (exitcode == 0) {
           failcnt = 0
           setTimeout(loop.next, 500)
@@ -90,13 +223,19 @@ exports.runlist = function (jobs, runList, user, _dryrun, onComplete) {
       }
 
       if (dryrun) {
-        onSpawn(jobname, -1)
+        const targetProps = jobs.depGraph.getNodeData(jobname)
+        const cmd = targetProps['exe'] || ''
+
         slaveLog(jobname, `[ dry run ] ${jobname}`)
-        onExit(jobname, 0)
+        onSpawn(cmd, -1)
+        onExit(cmd, 0)
         return
       }
 
-      exports.runjob(jobs, jobname, user, onSpawn, onExit)
+      exports.runjob(jobs, jobname, onSpawn, onExit, function () {
+        slaveLog(jobname, `[ test failed ] ${jobname} aborted`)
+        loop.brk()
+      })
     },
 
     /* done loop */
@@ -113,5 +252,6 @@ exports.runlist = function (jobs, runList, user, _dryrun, onComplete) {
   const jobs = await jobs_ldr.load('./test-jobs')
   const runList = await exports.getRunList(jobs, 'hello-world:say-helloworld')
   console.log(runList)
-  exports.runlist(jobs, runList, 'dm', 1)
+  //exports.runlist(jobs, runList, 1)
+  exports.runlist(jobs, runList)
 })()
