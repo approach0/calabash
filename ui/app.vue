6cc36b3ba97aba44a852e9c71205275f56b509eb
钟威 <zhongwei@dm-ai.cn>
2020-09-10 17:38:18 +0800

    finish application framework.

--- ui/app.vue
+++ ui/app.vue
@@ -1,5 +1,38 @@
 <template>
-<v-app>hello world</v-app>
+<v-app>
+
+  <v-app-bar color="deep-purple" dark app>
+    <template>
+    <v-app-bar-nav-icon>
+      <v-icon @click="drawer = !drawer">menu</v-icon>
+    </v-app-bar-nav-icon>
+
+    <v-toolbar-title>Approach0 Panel</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    rightmost
+    </template>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" color="purple darken-4 white--text" dark app>
+    <v-container>
+    drawer
+    </v-container>
+  </v-navigation-drawer>
+
+  <v-main app>
+
+    <v-container>
+    content
+    </v-container>
+  </v-main>
+
+  <v-footer color="deep-purple" dark app>
+  footer
+  </v-footer>
+
+</v-app>
 </template>
 
 <script>
@@ -8,6 +41,7 @@ import axios from 'axios'
 export default {
   data () {
     return {
+      drawer: false,
       debug: false,
     }
   },
