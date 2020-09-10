a21a85f5b0e77d656fb52ab3036855de37fda6f7
钟威 <zhongwei@dm-ai.cn>
2020-09-10 20:34:37 +0800

    writing UI.

--- ui/app.vue
+++ ui/app.vue
@@ -4,10 +4,10 @@
   <v-app-bar color="deep-purple" dark app>
     <template>
     <v-app-bar-nav-icon>
-      <v-icon @click="drawer = !drawer">menu</v-icon>
+      <v-icon @click="drawer = !drawer">featured_play_list</v-icon>
     </v-app-bar-nav-icon>
 
-    <v-toolbar-title>Approach0 Panel</v-toolbar-title>
+    <v-toolbar-title>Approach Zero Panel</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
@@ -15,9 +15,20 @@
     </template>
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer" color="purple darken-4 white--text" dark app>
+  <v-navigation-drawer width="600" v-model="drawer" color="purple darken-4 white--text" dark app>
     <v-container>
-    drawer
+
+        <v-text-field v-model="job_input" label="Run job" append-icon="sports" @click:append="clickRun" filled clearable>
+        </v-text-field>
+
+        <v-card class="grey darken-4 console" v-bind:loading="console_loading">{{console_content}}</v-card>
+
+        <v-row>
+          <v-chip class="ma-2" color="purple white--text">
+            <v-icon>star</v-icon> Hello
+          </v-chip>
+        </v-row>
+
     </v-container>
   </v-navigation-drawer>
 
@@ -37,11 +48,16 @@
 
 <script>
 import axios from 'axios'
+const port = 8964
 
 export default {
   data () {
     return {
       drawer: false,
+      job_input: '',
+      console_loading: false,
+      console_jobname: 'master',
+      console_content: '',
       debug: false,
     }
   },
@@ -53,15 +69,45 @@ export default {
   },
 
   mounted: function () {
+    let vm = this
+    setInterval(function () {
+      vm.fetch_log()
+    }, 1000)
   },
 
   methods: {
     random_idx(L) {
       return Math.floor(Math.random() * L)
     },
+
+    fetch_log() {
+      let vm = this
+      vm.console_loading = true
+
+      console.log(`http://0.0.0.0:${port}/get/log/_master_`)
+      axios.get(`http://0.0.0.0:${port}/get/log/_master_`)
+      .then(function (res) {
+        const data = res.data
+        vm.console_content = data['logdata']
+        vm.console_jobname = data['jobname']
+        vm.console_loading = false
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    },
+
+    clickRun() {
+    }
   }
 }
 </script>
 
 <style>
+.console {
+  white-space: pre-wrap;
+  overflow: auto;
+  font-size: 14px;
+  max-height: 900px;
+}
 </style>
