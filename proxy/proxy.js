aafda02afa1a68b38977f2595ae42e0a65c62b3b
钟威 <zhongwei@dm-ai.cn>
2020-09-11 20:27:16 +0800

    change default proxy public port

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const proxy = require('express-http-proxy')
 
-const port = 8080
+const port = 8089
 const http_dir = '../ui/dist'
 
 process.on('SIGINT', function() {
