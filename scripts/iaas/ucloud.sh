15546c9945ad0e81472af8dac93f8ed5a39054f7
t-k- <clock126@126.com>
2020-09-28 16:02:10 +0800

    update scripts/iaas/ucloud.sh

--- scripts/iaas/ucloud.sh
+++ scripts/iaas/ucloud.sh
@@ -8,20 +8,28 @@ source $SCRIPT_PATH/../common.env.sh
 UCLOUD_CLI_IMG=${DOCKER_MIRROR}ga6840/ucloud-cli:latest
 UCLOUD_CLI="$DOCKER run -it $UCLOUD_CLI_IMG /root/wrap-run.sh $UCLOUD_CLI_PUBKEY $UCLOUD_CLI_PRIKEY"
 
-ucloud_list_nodes() {
-  $UCLOUD_CLI uhost list --region cn-gd
+ucloud_node_list() {
+  $UCLOUD_CLI --json uhost list --region cn-gd
 }
 
-ucloud_list_regions() {
+ucloud_regions() {
   $UCLOUD_CLI region
 }
 
 ucloud_node_create() {
   PASSWD=$1
-  $UCLOUD_CLI uhost create --cpu 1 --memory-gb 1 --password $PASSWD --image-id uimage-j0r4vh --region cn-gd --zone cn-gd-02 --charge-type 'Dynamic'
+  $UCLOUD_CLI uhost create --cpu 1 --memory-gb 1 --password $PASSWD --image-id uimage-j0r4vh --region cn-gd --zone cn-gd-02 --charge-type 'Dynamic' --firewall-id firewall-njshvwlr --vpc-id uvnet-touf2ybp --subnet-id subnet-b2ksux2q --create-eip-bandwidth-mb 1 --name 'calabash_node'
 }
 
 ucloud_node_delete() {
   HOSTID=$1
   $UCLOUD_CLI uhost delete --region cn-gd --yes --uhost-id $HOSTID
+}
+
+ucloud_account_balance() {
+  $UCLOUD_CLI api --Action DescribeAccountSummary
+}
+
+ucloud_node_set_label() {
+  $UCLOUD_CLI api --Action ModifyUHostInstanceRemark --Region cn-gd --UHostId uhost-2nbwal51 --Remark 'dog=0,cat=1,name=Kitty'
 }
