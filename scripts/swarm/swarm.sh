b823a8f81f0e0dec822af55207569fe592f6f028
t-k- <clock126@126.com>
2020-10-15 12:30:39 +0800

    minor printing.

--- scripts/swarm/swarm.sh
+++ scripts/swarm/swarm.sh
@@ -156,6 +156,7 @@ swarm_service_create() {
 	# get "list" variables
 	constraints=$(eval echo $(for c in ${!constraints_@}; do echo -n "--constraint=\$$c "; done))
 	mounts=$(eval echo $(for m in ${!mounts_@}; do echo -n "--mount=\$$m "; done))
+	echo '[[[ swarm_service_update_configs ]]]'
 	configs=`swarm_service_update_configs $servName`
 
 	# print service arguments
