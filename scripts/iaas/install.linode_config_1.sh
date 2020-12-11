1bf35666c6e6cc82ace70a3c55fcc22a6bc8f843
t-k- <clock126@126.com>
2020-12-11 16:14:26 +0800

    do not drop cache and swap, buy memory.

--- scripts/iaas/install.linode_config_1.sh
+++ scripts/iaas/install.linode_config_1.sh
@@ -42,10 +42,11 @@ apt-get install -y -qq --no-install-recommends atop smem ncdu
 # smem usage: smem -pkw
 
 # add CRON jobs for regularly memory/disk cleanup
-(crontab -l ; echo '30 1 * * * sync; echo 1 > /proc/sys/vm/drop_caches') | sort - | uniq - | crontab -
-(crontab -l ; echo '40 1 * * * swapoff -a; swapon -a') | sort - | uniq - | crontab -
 (crontab -l ; echo '50 1 * * * docker system prune --force') | sort - | uniq - | crontab -
 (crontab -l ; echo '60 1 * * * docker image prune -a --force') | sort - | uniq - | crontab -
+
+#(crontab -l ; echo '30 1 * * * sync; echo 1 > /proc/sys/vm/drop_caches') | sort - | uniq - | crontab -
+#(crontab -l ; echo '40 1 * * * swapoff -a; swapon -a') | sort - | uniq - | crontab -
 
 crontab -l
 # tail -1000 /var/log/syslog | grep CRON
