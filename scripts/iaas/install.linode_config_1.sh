8e14895954ce935dd691bd1bb8aeb0f2d988d4ad
t-k- <clock126@126.com>
2020-12-13 17:54:25 +0800

    fix cronjob format

--- scripts/iaas/install.linode_config_1.sh
+++ scripts/iaas/install.linode_config_1.sh
@@ -43,10 +43,10 @@ apt-get install -y -qq --no-install-recommends atop smem ncdu
 
 # add CRON jobs for regularly memory/disk cleanup
 (crontab -l ; echo '50 1 * * * docker system prune --force') | sort - | uniq - | crontab -
-(crontab -l ; echo '60 1 * * * docker image prune -a --force') | sort - | uniq - | crontab -
+(crontab -l ; echo '59 1 * * * docker image prune -a --force') | sort - | uniq - | crontab -
 
 #(crontab -l ; echo '30 1 * * * sync; echo 1 > /proc/sys/vm/drop_caches') | sort - | uniq - | crontab -
 #(crontab -l ; echo '40 1 * * * swapoff -a; swapon -a') | sort - | uniq - | crontab -
 
-crontab -l
+crontab -l # list cron jobs
 # tail -1000 /var/log/syslog | grep CRON
