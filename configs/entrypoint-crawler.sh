42cee678a36025b3e3b7db56ab3024c95faebac9
t-k- <clock126@126.com>
2020-12-21 16:15:26 +0800

    add more AoPS data sources

--- configs/entrypoint-crawler.sh
+++ configs/entrypoint-crawler.sh
@@ -42,31 +42,49 @@ while true; do
 		;;
 
 		10)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 3
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 3 # Middle School
 		;;
 
 		11)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 4
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 4 # High School
 		;;
 
 		12)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 5
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 5 # Contests and Programs
 		;;
 
 		13)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 6
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 6 # High School Olympiads
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 1068820 # Old High School Olympiads
 		;;
 
 		14)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 7
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 7 # College Math
 		;;
 
 		15)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 123
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 163 # Computer Science and Info.
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 164 # Physics
 		;;
 
 		16)
-			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 163
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 129 # American Regions Math League (ARML)
+		;;
+
+		17)
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 594864 # AoPS Mock Contests
+		;;
+
+		18)
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 123 # USA Math Talent Search (USMTS)
+		;;
+
+		19)
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 140 # PUMaC (Princeton)
+		;;
+
+		20)
+			crawler-artofproblemsolving.com.py -n 0 -o 3650 -c 129 # Harvard-MIT (HMMT)
 		;;
 
 		*)
